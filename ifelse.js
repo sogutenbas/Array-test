f471f5288ffa6af4c3fb69e34fca35458be29abe
serhat oguten <sogutenbas@gmail.com>
2021-02-25 23:52:07 +0100

    first commit

---
+++ ifelse.js
@@ -0,0 +1,15 @@
+let name = ["dali" , "hamza", "serhat"]
+let number=[26, 14, 66]
+let option="dali"
+if(option=="dali"){
+    console.log((number[0]+number[1]+number[2])/3)
+}
+else if(option=="hamza"){
+    console.log(number[0]+number[1]+number[2])
+}
+else if(option=="serhat"){  
+    console.log(number[0])
+}
+else{
+    console.log("Hatali giris yaptiniz!")
+}
